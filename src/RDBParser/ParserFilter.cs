f3257aceb93f3702c9cc42533d8122c6d03d8377
catcherwong <catcher_hwq@outlook.com>
2022-03-24 22:36:53 +0800

    feat(lib): filter comments

--- src/RDBParser/ParserFilter.cs
+++ src/RDBParser/ParserFilter.cs
@@ -4,10 +4,14 @@ namespace RDBParser
 {
     public class ParserFilter
     {
+        /// <summary>
+        /// The databases that you need
+        /// </summary>
         public List<int> Databases { get; set; }
 
+        /// <summary>
+        /// The types that you need
+        /// </summary>
         public List<string> Types { get; set; }
-
-        public List<byte[]> Keys { get; set; }
     }
 }
