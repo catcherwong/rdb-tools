c077b5190a0b5c0911ba15a7ee7602cb3aab7aaf
catcherwong <catcher_hwq@outlook.com>
2022-12-03 22:00:03 +0800

    feat(cli): keys cmd support prefix filter

--- src/RDBCli/Callbacks/KeysOnlyCallback.cs
+++ src/RDBCli/Callbacks/KeysOnlyCallback.cs
@@ -1,16 +1,52 @@
 ﻿using RDBParser;
 using System.Collections.Generic;
 using System.CommandLine;
+using System.Linq;
 
 namespace RDBCli.Callbacks
 {
     internal class KeysOnlyCallback : RDBParser.IReaderCallback
     {
         private IConsole _console;
+        private List<string> _prefixes;
 
-        public KeysOnlyCallback(IConsole console)
+        public KeysOnlyCallback(IConsole console, List<string> prefixes)
         {
             this._console = console;
+            this._prefixes = prefixes;
+        }
+
+        private void OutputInfo(byte[] key)
+        {
+            var keyStr = System.Text.Encoding.UTF8.GetString(key);
+
+            if(CheckPreifx(keyStr))
+            {
+                _console.WriteLine(keyStr);
+            }
+        }
+
+        private bool CheckPreifx(string key)
+        {
+            var flag = false;
+
+            if(_prefixes != null && _prefixes.Any())
+            {
+                foreach(var item in _prefixes)
+                {
+                    if(key.StartsWith(item))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                flag = true;
+            }
+
+            return flag;
         }
 
         public void AuxField(byte[] key, byte[] value)
@@ -75,7 +111,7 @@ namespace RDBCli.Callbacks
 
         public void Set(byte[] key, byte[] value, long expiry, Info info)
         {
-            _console.WriteLine(System.Text.Encoding.UTF8.GetString(key));
+            OutputInfo(key);
         }
 
         public void StartDatabase(int database)
@@ -84,18 +120,18 @@ namespace RDBCli.Callbacks
 
         public void StartHash(byte[] key, long length, long expiry, Info info)
         {
-            _console.WriteLine(System.Text.Encoding.UTF8.GetString(key));
+            OutputInfo(key);
         }
 
         public void StartList(byte[] key, long expiry, Info info)
         {
-            _console.WriteLine(System.Text.Encoding.UTF8.GetString(key));
+            OutputInfo(key);
         }
 
         public bool StartModule(byte[] key, string module_name, long expiry, Info info)
         {
             if (key != null && key.Length > 0)
-                _console.WriteLine(System.Text.Encoding.UTF8.GetString(key));
+                OutputInfo(key);
 
             return false;
         }
@@ -106,16 +142,17 @@ namespace RDBCli.Callbacks
 
         public void StartSet(byte[] key, long cardinality, long expiry, Info info)
         {
+            OutputInfo(key);
         }
 
         public void StartSortedSet(byte[] key, long length, long expiry, Info info)
         {
-            _console.WriteLine(System.Text.Encoding.UTF8.GetString(key));
+            OutputInfo(key);
         }
 
         public void StartStream(byte[] key, long listpacks_count, long expiry, Info info)
         {
-            _console.WriteLine(System.Text.Encoding.UTF8.GetString(key));
+            OutputInfo(key);
         }
 
         public void StreamListPack(byte[] key, byte[] entry_id, byte[] data)
