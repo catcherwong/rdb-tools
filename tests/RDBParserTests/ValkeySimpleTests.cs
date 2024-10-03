702d263a0045c4a32276996751b4b1be96cc2189
Catcher Wong <catcher_hwq@outlook.com>
2024-10-03 23:41:21 +0800

    add method to check is valkey (#72)
    Signed-off-by: catcherwong <catcher_hwq@outlook.com>

---
+++ tests/RDBParserTests/ValkeySimpleTests.cs
@@ -0,0 +1,32 @@
+﻿using RDBParser;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RDBParserTests
+{
+    public class ValkeySimpleTests
+    {
+        private ITestOutputHelper _output;
+
+        public ValkeySimpleTests(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+
+        [Fact]
+        public void TestIsValkey()
+        {
+            // set mykey v1
+            // bgsave
+            var path = TestHelper.GetRDBPath("valkey_80_normal.rdb");
+
+            var callback = new TestReaderCallback(_output);
+            var parser = new BinaryReaderRDBParser(callback);
+            parser.Parse(path);
+
+
+            Assert.True(parser.IsValkey());
+        }
+    }
+
+}
